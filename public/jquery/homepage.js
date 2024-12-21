618cd613637e0910287721039a26df979f436781
crkntk <crkntk@gmail.com>
2024-12-20 16:06:58 -0800

    button fix only the css not the ajax call

--- public/jquery/homepage.js
+++ public/jquery/homepage.js
@@ -3,35 +3,6 @@
 
 console.log("IT LOADED!!!!!");
 
-$(".btn-delete").hover(
-    function() {
-        $(this).css("background-color", "#D71313");
-        $(this).css("color", "white");
-    },
-    function() {
-        $(this).css("background-color", "white");
-        $(this).css("color", "#D71313");
-    }
-);
-$(".btn-delete").on("mouseenter",function() {
-        $(this).css("background-color", "#D71313");
-        $(this).css("color", "white");
-    }).on("mouseleave", function() {
-        $(this).css("background-color", "white");
-        $(this).css("color", "#D71313");
-    }
-);
-
-$(".btn-outline-update").hover(
-    function() {
-        $(this).css("background-color", "#378CE7");
-        $(this).css("color", "white");
-    },
-    function() {
-        $(this).css("background-color", "white");
-        $(this).css("color", "#378CE7");
-    }
-);
 
 $(".btn-delete").on("click",function() {
     var usern = $("h1").attr("id");
